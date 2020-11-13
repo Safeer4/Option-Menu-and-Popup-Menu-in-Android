8fb11d8d7d0e182f67682703d8ab2e64a55e6cb6
safeerullah <safeerafridi9@gmail.com>
2020-11-13 19:50:18 +0500

    Added comments

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    
+    //This function is to create option menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //This function is for making the option menu clickable
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.item1)
@@ -53,11 +55,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //This is for option menu, this function will trigger once the gadget against this
+    //function is clicked.
     public void btnPopup(View view) {
         PopupMenu popup = new PopupMenu(this, view);
         popup.getMenuInflater().inflate(R.menu.main_menu, popup.getMenu());
         popup.show();
+	
 
+	//This function is to make the popup menu clickable
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
